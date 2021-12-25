c18c4fedea2b0f2ff36b055703e0e3af6d2e3266
Evgeniy <maximov.evgen@gmail.com>
2021-12-25 04:24:38 +0300

    Scripted auto-commit on change (2021-12-25 04:24:38) by gitwatch.sh

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -2,6 +2,7 @@ public class Server {
     private static final String SERVER = "localhost";
     private static final Integer PORT = 8080;
     private static final String TEST_URL = "testUrl";
+    private static final String COUNT = "count";
     private static final Integer TIME_OUT_MILLIS = 5000;
 
 }

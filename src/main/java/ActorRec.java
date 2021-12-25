6bb0cfd1cc1ce3b4cc10c408e944595b92099c92
Evgeniy <maximov.evgen@gmail.com>
2021-12-25 04:36:27 +0300

    Scripted auto-commit on change (2021-12-25 04:36:27) by gitwatch.sh

--- src/main/java/ActorRec.java
+++ src/main/java/ActorRec.java
@@ -5,7 +5,7 @@ import akka.japi.pf.ReceiveBuilder;
 import java.util.HashMap;
 
 public class ActorRec extends AbstractActor {
-    private final HashMap<String, Integer> storage = new HashMap<>;
+    private final HashMap<String, Integer> storage = new HashMap<>();
 
     public Receive createReceive() {
         return ReceiveBuilder

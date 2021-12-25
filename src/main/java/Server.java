3ec82b2347e65a09da88ee582a76682bbdcf170a
Evgeniy <maximov.evgen@gmail.com>
2021-12-25 04:38:24 +0300

    Scripted auto-commit on change (2021-12-25 04:38:24) by gitwatch.sh

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -20,6 +20,13 @@ public class Server {
         ActorRef actor = system.actorOf(Props.create(ActorRec.class));
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute().flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost(SERVER, PORT),
+                materializer
+        );
     }
 
 }

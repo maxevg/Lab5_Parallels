9e18bf8c7bc6f8b431c81751cf8e08bb28daeb09
Evgeniy <maximov.evgen@gmail.com>
2021-12-25 04:47:04 +0300

    Scripted auto-commit on change (2021-12-25 04:47:04) by gitwatch.sh

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -42,6 +42,8 @@ public class Server {
                     Query q = req.getUri().query();
                     String url = q.get(TEST_URL).get();
                     int count = Integer.parseInt(q.get(COUNT).get());
+                    System.out.println(url + " " + count);
+                    return new Pair<String, Integer>(url, count);
                 })
     }
 

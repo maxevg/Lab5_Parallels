284b0e4e7e8e1df098ee25c4ea3fb710fb731f3b
Evgeniy <maximov.evgen@gmail.com>
2021-12-25 04:43:24 +0300

    Scripted auto-commit on change (2021-12-25 04:43:24) by gitwatch.sh

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -2,12 +2,17 @@ import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.model.Query;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 import java.io.IOException;
+import java.util.concurrent.CompletionStage;
 
 public class Server {
     private static final String SERVER = "localhost";
@@ -29,6 +34,14 @@ public class Server {
                 ConnectHttp.toHost(SERVER, PORT),
                 materializer
         );
+    }
+
+    private static Flow<HttpRequest, HttpResponse, NotUsed> createFlow(Http http, ActorSystem system, ActorMaterializer materializer, ActorRef actor) {
+        return  Flow.of(HttpRequest.class)
+                .map((req) -> {
+                    Query q = req.getUri().query();
+                    String url = q.get(TEST_URL).get();
+                })
     }
 
 }
